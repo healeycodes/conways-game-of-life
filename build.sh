2ab4b4238fe6ee952f36fffe20a3184e275f8e65
Jasper Wong <wlcjasper@gmail.com>
2024-10-12 15:22:46 +0100

    Remove in favour of Dockerfile

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Install X11 development libraries
-sudo apt-get update -y
-sudo apt-get install -y libx11-dev libxrandr-dev libxcursor-dev libxi-dev mesa-common-dev libgl1-mesa-dev libglu1-mesa-dev
-
-# Build the Go application
-go build -tags netgo -ldflags '-s -w' -o app .
